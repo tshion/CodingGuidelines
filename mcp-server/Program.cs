c0021ac98d102192b8afc82888a00c2ffc6fffe5
tshion <tenta_shion@yahoo.co.jp>
2025-11-15 18:11:22 +0900

    HTTP ベースに書き換え

--- mcp-server/Program.cs
+++ mcp-server/Program.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
 
-var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMcpServer()
-    .WithStdioServerTransport()
+    .WithHttpTransport()
     .WithToolsFromAssembly();
-
 var app = builder.Build();
 
-await app.RunAsync();
+app.MapMcp();
+
+app.Run();
